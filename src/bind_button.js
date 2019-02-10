bbc7bc174be26205c0adc0eb275bdd97ce64eff1
hieki <ksakahieki@gmail.com>
2019-02-10 20:03:32 +0900

    Add Super Yokubari Set

--- src/bind_button.js
+++ src/bind_button.js
@@ -1,10 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = Array.from(document.getElementsByClassName("btn"));
-  buttons.forEach(button => {
+"use strict";
+
+const allButtons = () => {
+  return Array.from(document.getElementsByClassName("btn"));
+}
+
+const bindButtons = () => {
+  allButtons().forEach(button => {
     button.addEventListener("click", e => {
       const audio = document.getElementById(`audio-${e.target.id}`);
       audio.currentTime = 0;
       audio.play();
     });
   });
+}
+
+const bindEverythingButton = () => {
+  document.getElementById("everything").addEventListener("click", e => {
+    allButtons().forEach(button => {
+      button.click();
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  bindButtons();
+  bindEverythingButton();
 });
